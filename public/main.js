4b604c732a4e70776185271c5a88f7b73a834763
Kleanthismits <kleanthismits@hotmail.gr>
2021-01-23 19:33:14 +0200

    first commit

---
+++ public/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $(".sidenav").sidenav();
+    $(".materialboxed").materialbox();
+    $(".tabs").tabs();
+    $(".tooltipped").tooltip();
+    $(".scrollspy").scrollSpy();
+});

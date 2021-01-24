d44c12eef27cb9685dbbf61d3160584160499042
Kleanthismits <kleanthismits@hotmail.gr>
2021-01-24 23:19:13 +0200

    second

--- public/main.js
+++ public/main.js
@@ -1,7 +1,28 @@
-$(document).ready(function() {
-    $(".sidenav").sidenav();
-    $(".materialboxed").materialbox();
-    $(".tabs").tabs();
-    $(".tooltipped").tooltip();
-    $(".scrollspy").scrollSpy();
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+  $(".materialboxed").materialbox();
+  $(".tabs").tabs();
+  $(".tooltipped").tooltip();
+  $(".scrollspy").scrollSpy();
+  $("#current_year").text(new Date().getFullYear());
+  $("body").click((e) => {
+    e.preventDefault();
+    var source_class = $(e.target).attr("class");
+    switch (source_class) {
+      case "scroll_source":
+        scrollToElement(e.target.id + "-section");
+        break;
+      default:
+        break;
+    }
+  });
 });
+function scrollToElement(elementId) {
+  //get the element
+  var scroll_target = $("#" + elementId);
+  if (scroll_target.offset()) {
+    //get the vertical distance of the element from the top of the page
+    var verticalPositionOfElement = scroll_target.offset().top;
+    $(window).scrollTop(verticalPositionOfElement - 70);
+  }
+}
